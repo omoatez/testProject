254b78ea4c9163ffeed5acf8f8efe17eaad7fb7d
moatez.ouertatani <moatez.ouertatani@adesso.de>
2024-09-11 19:24:59 +0200

    new SSuffices tactic

--- TestProject.lean
+++ TestProject.lean
@@ -10,10 +10,31 @@ open Tactic
 open Command
 open Term
 open Lean Linter
+open Meta
+open PrettyPrinter
 
 register_option linter.structureProof : Bool := {
   defValue := true
 }
+
+syntax (name := showQuestion) "show?" : tactic
+
+@[tactic showQuestion] def evalShowQuestion : Tactic := fun _stx => do
+  let goal ← getMainGoal
+  let goalType ← goal.getType
+  let goalTypeString ← PrettyPrinter.delab goalType
+
+  let suggestionText : SuggestionText := "apply some_tactic"
+  let suggestion : Suggestion := {
+    suggestion := suggestionText,
+    preInfo? := some "Try this tactic: ",
+    postInfo? := none,
+    style? := none,
+    messageData? := none,
+    toCodeActionTitle? := none
+  }
+  Lean.Meta.Tactic.TryThis.addSuggestion _stx suggestion
+  return ()
 
 def suggestSimplifiedHaveSyntax  (stx : Syntax) (infoTrees : Array InfoTree) : TermElabM Unit := do
   match stx with
@@ -41,7 +62,33 @@ def suggestSimplifiedHaveSyntax  (stx : Syntax) (infoTrees : Array InfoTree) : T
       | _ => logLint linter.structureProof stx "no TermInfo"
   | _ => pure ()
 
+ -- TODO refactor simp??
+syntax (name := simp??) "simp??" : tactic
 
+@[tactic simp??] def evalSimpQuestion : Tactic := fun stx => do
+  -- Get the current goal and its type
+  let goal ← getMainGoal
+  let goalType ← goal.getType
+
+  -- Apply the simpGoal function
+  let (simplifiedResult, usedSimps) ← simpGoal goal {}
+
+  match simplifiedResult with
+  | none =>
+    logInfo "simp made no progress"
+  | some (_, newMVarId) =>
+    if usedSimps.isEmpty then
+      logInfo "simp made no progress"
+    else
+      let U ← mkFreshExprMVar (← Lean.MVarId.getType newMVarId)
+      let suggestionText ← `(tactic | suffices $(mkIdent `hyp):ident : _ by simp)
+      Lean.Meta.Tactic.TryThis.addSuggestion stx suggestionText
+      let subgoal1 ← mkFreshExprMVar (← Lean.MVarId.getType newMVarId)
+      replaceMainGoal [newMVarId, subgoal1.mvarId!]
+
+      setGoals [newMVarId]
+      evalTactic (← `(tactic| simp))
+      pure ()
 
 def iterateAndSuggest(code: Syntax): CommandElabM Unit := do
   if !linter.structureProof.get (← getOptions) then
@@ -54,6 +101,25 @@ def iterateAndSuggest(code: Syntax): CommandElabM Unit := do
   for (_,tactic) in tacticMap do
     liftTermElabM (suggestSimplifiedHaveSyntax tactic infoTrees)
   pure ()
+
+syntax (name := SSuffices) "SSuffices " term " by" tactic : tactic
+
+@[tactic SSuffices] def evalSSuffices : Tactic := fun stx => do
+  match stx with
+  | `(tactic| SSuffices $expectedType by $tactic) => do
+    -- Get the current goal and its type
+    evalTactic tactic
+    let goals ← getGoals
+    if (goals.length > 1) then throwError "more than one goal"
+    if goals.length = 0 then throwError "no goal"
+    let firstGoal := goals[0]!
+    withMainContext do
+      let expectedTypeTerm ← Tactic.elabTerm expectedType none
+      let goalType ← firstGoal.getType
+      let isEqual ← isDefEq goalType expectedTypeTerm
+      if (!isEqual) then throwError "different Type"
+  | _ => throwUnsupportedSyntax
+
 
 def structureProofLinter : Linter where
   run := iterateAndSuggest
--- testSuggestion.lean
+++ testSuggestion.lean
@@ -1,5 +1,5 @@
-import TestProject
 import Lean
+import «TestProject»
 set_option linter.structureProof true
 theorem example_theorem (a b : Nat) : a + b = b + a := by
   have h₁ := Nat.add_comm a b
@@ -16,3 +16,16 @@ example : 2 + 2 = 4 := by
   let x := 2 + 2
   show x = 4
   rfl
+
+example (n : Nat) : n + 0 = n := by
+  SSuffices n = n by simp
+
+example (n m : Nat) (h : n + m = m + n) : (n + 0) + m = m + (n + 0) := by
+  SSuffices n + m = m + n by simp
+
+example (a b : Nat) : a + b = b + a := by
+  apply some_tactic
+  rw [Nat.add_comm]
+
+example : T := by
+  simp??
