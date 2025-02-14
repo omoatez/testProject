36b3c66dd92cf75d965678bdf81ba472bb999a04
moatez.ouertatani <moatez.ouertatani@adesso.de>
2025-02-14 22:54:42 +0100

    added mathlib to the dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -9,3 +9,5 @@ lean_lib «TestProject» where
   -- add library configuration options here
 require aesop from git
   "https://github.com/leanprover-community/aesop" @ "v4.12.0"
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "v4.12.0"
